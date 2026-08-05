eeecb821ff0ad0fb0f0239af0ddc6edaee98918f
giorgi.page <andreagiorgino1@gmail.com>
2026-08-05 08:49:17 +0200

    copy and move json ctors implemnted

--- src/libjson/json.cxx
+++ src/libjson/json.cxx
@@ -43,6 +43,32 @@ auto _parse_value(std::istream& is) -> json;
 
 // ----------------------------------------------------------------------------
 
+json::json(const json& rhs)
+    : _hasValue(rhs._hasValue),
+      _value(_deep_copy(rhs._value)) {}
+
+auto json::operator =(const json& rhs) -> json& {
+    if (this != &rhs) {
+        _hasValue = rhs._hasValue;
+        _value    = _deep_copy(rhs._value);
+    }
+
+    return *this;
+}
+
+json::json(json&& rhs)
+    : _hasValue(rhs._hasValue),
+      _value(std::move(rhs._value)) {
+    rhs._hasValue = false;
+}
+
+auto json::operator =(json&& rhs) -> json& {
+    _hasValue     = rhs._hasValue;
+    _value        = std::move(rhs._value);
+    rhs._hasValue = false;
+    return *this;
+}
+
 auto json::has_value(void) const noexcept -> bool {
     return _hasValue;
 }
