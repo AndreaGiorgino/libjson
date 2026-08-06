6fd9ce8b66655cb1f61bb6c8060240c467278c05
giorgi.page <andreagiorgino1@gmail.com>
2026-08-06 16:15:30 +0200

    test array push data implemented

---
+++ CTest/TestArrayPushValue.cxx
@@ -0,0 +1,36 @@
+#include "helpers.hxx"
+#include "libjson/json.hxx"
+
+using json     = libjson::json;
+using array_t = libjson::array_t;
+
+auto TestArrayPushValue(int, char**) -> int {
+    const std::string expected {"json"};
+
+    // array initialised
+    json el0 {array_t {}};
+    el0.push_back(expected);
+    helpers::check_child_eq(el0, 0, expected);
+
+    // null initialised
+    json el1 {};
+    el1.push_back(expected);
+    helpers::check_child_eq(el1, 0, expected);
+
+    // move
+    auto buffer {expected};
+
+    json el2 {};
+    el2.push_back(std::move(buffer));
+    helpers::check_child_eq(el2, 0, expected);
+
+    if (!buffer.empty()) throw "buffer not moved";
+
+    // trasform and push
+    json el3 {69};
+    el3.push_back(expected);
+    helpers::check_child_eq(el3, 0, 69);
+    helpers::check_child_eq(el3, 1, expected);
+
+    return 0;
+}
