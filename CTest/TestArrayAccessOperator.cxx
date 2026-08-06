b6ac14ade69d98b0fc99eafda50a80d094e17e4c
giorgi.page <andreagiorgino1@gmail.com>
2026-08-06 16:37:25 +0200

    test array access operator implemented

---
+++ CTest/TestArrayAccessOperator.cxx
@@ -0,0 +1,24 @@
+#include "libjson/json.hxx"
+
+using json    = libjson::json;
+using array_t = libjson::array_t;
+
+auto TestArrayAccessOperator(int, char**) -> int {
+    // bad_variant_access
+    try {
+        (void)json {69}[0];
+    } catch (const std::bad_variant_access&) {
+    } catch (...) {
+        throw;
+    }
+
+    // out_of_range
+    try {
+        (void)json {array_t {}}[0];
+    } catch (const std::out_of_range&) {
+    } catch (...) {
+        throw;
+    }
+
+    return 0;
+}
