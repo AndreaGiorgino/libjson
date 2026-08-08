96c4011bc74262114557e97804865bbf1bbec367
giorgi.page <andreagiorgino1@gmail.com>
2026-08-08 17:35:57 +0200

    test decode sample data implemented

---
+++ CTest/TestDecodeSampleData.cxx
@@ -0,0 +1,34 @@
+#include <fstream>
+
+#include "helpers.hxx"
+#include "libjson/extra/decode.hxx"
+
+using object_t = libjson::object_t;
+
+auto TestDecodeSampleData(int, char**) -> int {
+    const object_t expected {
+        {"lastName",  "ABBING"    },
+        {"firstName", "Mr Anthony"},
+        {"age",       "41"        },
+        {"paxClass",  "3rd Class" },
+        {"passenger", true        },
+        {"survivor",  false       }
+    };
+
+    std::ifstream ifs {"TestFiles/titanic-passengers.json"};
+
+    const auto root {libjson::decode(ifs)};
+
+    helpers::check_eq<std::string>(
+        root.at(0).at("lastName"), expected.at("lastName"));
+    helpers::check_eq<std::string>(
+        root.at(0).at("firstName"), expected.at("firstName"));
+    helpers::check_eq<std::string>(root.at(0).at("age"), expected.at("age"));
+    helpers::check_eq<std::string>(
+        root.at(0).at("paxClass"), expected.at("paxClass"));
+    helpers::check_eq<bool>(
+        root.at(0).at("passenger"), expected.at("passenger"));
+    helpers::check_eq<bool>(root.at(0).at("survivor"), expected.at("survivor"));
+
+    return 0;
+}
