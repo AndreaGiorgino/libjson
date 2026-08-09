72d668468b8b04e652fc8a192a3ac1cc37dba381
giorgi.page <andreagiorgino1@gmail.com>
2026-08-09 19:09:30 +0200

    example updated

--- example/src/main.cxx
+++ example/src/main.cxx
@@ -1,3 +1,25 @@
+#include <iostream>
+#include <sstream>
+
+#include "libjson/extra/decode.hxx"
+
+using namespace libjson;
+
 auto main(int, char**) -> int {
+    std::stringstream ss {R"(
+    {
+        "name": "John Smith",
+        "age": 25,
+        "city": {
+            "name": "London"
+        }
+    }
+    )"};
+
+    auto root {libjson::decode(ss)};
+    std::cout << root["name"].as<std::string>() << " from "
+              << root["city"]["name"].as<std::string>() << " is "
+              << root["age"].as<int>() << " years old." << std::endl;
+
     return 0;
 }
