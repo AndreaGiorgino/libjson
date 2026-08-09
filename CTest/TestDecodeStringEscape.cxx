e2f8a781763d1abdfad70a1dde67605a86315895
giorgi.page <andreagiorgino1@gmail.com>
2026-08-09 18:17:04 +0200

    test decode string escape fixed

--- CTest/TestDecodeStringEscape.cxx
+++ CTest/TestDecodeStringEscape.cxx
@@ -1,4 +1,6 @@
 #include <sstream>
+#include <iostream>
+#include <print>
 
 #include "helpers.hxx"
 #include "libjson/extra/decode.hxx"
@@ -21,15 +23,15 @@ auto TestDecodeStringEscape(int, char**) -> int {
     )"};
 
     const auto root {libjson::decode(ss)};
-    helpers::check_eq(root.at("quote"), "\\\""s);
-    helpers::check_eq(root.at("backslash"), "\\\\"s);
-    helpers::check_eq(root.at("forwardSlash"), "\\/"s);
-    helpers::check_eq(root.at("backspace"), "\\b"s);
-    helpers::check_eq(root.at("formFeed"), "\\f"s);
-    helpers::check_eq(root.at("newline"), "\\n"s);
-    helpers::check_eq(root.at("carriageReturn"), "\\r"s);
-    helpers::check_eq(root.at("tab"), "\\t"s);
-    helpers::check_eq(root.at("unicode"), "\\u03A0"s);
+    helpers::check_eq(root.at("quote"), "\""s);
+    helpers::check_eq(root.at("backslash"), "\\"s);
+    helpers::check_eq(root.at("forwardSlash"), "/"s);
+    helpers::check_eq(root.at("backspace"), "\b"s);
+    helpers::check_eq(root.at("formFeed"), "\f"s);
+    helpers::check_eq(root.at("newline"), "\n"s);
+    helpers::check_eq(root.at("carriageReturn"), "\r"s);
+    helpers::check_eq(root.at("tab"), "\t"s);
+    helpers::check_eq(root.at("unicode"), "\u03A0"s);
 
     return 0;
 }
