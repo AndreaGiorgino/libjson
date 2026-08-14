72baa7fdc36d45d69200ca2be245da97869a1734
giorgi.page <andreagiorgino1@gmail.com>
2026-08-14 19:29:13 +0200

    docs updated

--- include/libjson/json.hxx
+++ include/libjson/json.hxx
@@ -165,7 +165,7 @@ class json final {
     // ---------- capacity ----------
 
     /**
-     * @brief Checks wether the container is empty. If the stored value is not
+     * @brief Check wether the container is empty. If the stored value is not
      * a container returns false, if no value is being stored returns true
      */
     [[nodiscard]] auto empty(void) const noexcept -> bool;
@@ -187,7 +187,7 @@ class json final {
     [[nodiscard]] auto has_value(void) const noexcept -> bool;
 
     /**
-     * @brief Get the stored value as Tp if possible (primitive)
+     * @brief Get the stored value as Tp or throws (primitive)
      *
      * @tparam Tp The target type
      * @return The stored value of type Tp
@@ -205,7 +205,7 @@ class json final {
     }
 
     /**
-     * @brief Get the stored value as Tp if possible (pointer allocated)
+     * @brief Get the stored value as Tp or throws (pointer allocated)
      *
      * @tparam Tp The target type
      * @return The stored value of type Tp
@@ -244,16 +244,17 @@ class json final {
     }
 
     /**
-     * @brief Check wether the container contains element with specific key. If
-     * the value being stored is not an object value return false
+     * @brief Check wether the container contains an element with the
+     * specificied key. If the value being stored is not an object value return
+     * false
      *
      * @param key The key to check
      */
     [[nodiscard]] auto contains(std::string_view key) const noexcept -> bool;
 
     /**
-     * @brief Get a reference to the element in the stored array at the
-     * specified location
+     * @brief Get a const reference to the element in the stored array at the
+     * specified location or throws
      *
      * @param index The position of the element
      * @return The reference to the element
@@ -265,8 +266,8 @@ class json final {
     [[nodiscard]] auto at(std::size_t index) const -> const json&;
 
     /**
-     * @brief Get a reference to the element in the stored object with the
-     * specified key
+     * @brief Get a const reference to the element in the stored object with the
+     * specified key or throws
      *
      * @param key The key of the element
      * @return The reference to the element
@@ -278,27 +279,13 @@ class json final {
      */
     [[nodiscard]] auto at(std::string_view key) const -> const json&;
 
-    /**
-     * @brief Get a reference to the element in the stored object with the
-     * specified key. Insert it if it does not exist
-     *
-     * @param key The key of the element
-     * @return The reference to the element
-     *
-     * @throws std::runtime_error If no value is being stored
-     * @throws std::bad_variant_access If the stored value is not an object
-     * @throws std::out_of_range If the object does not have an element with the
-     * specified key
-     */
-    [[nodiscard]] auto operator [](std::string_view key) -> json&;
-
     // ------------------------------
 
     // --------- modifiers ----------
 
     /**
      * @brief Get a reference to the element in the stored array at the
-     * specified location
+     * specified location or throws
      *
      * @param index The position of the element
      * @return The reference to the element
@@ -310,14 +297,31 @@ class json final {
     [[nodiscard]] auto operator [](std::size_t index) -> json&;
 
     /**
-     * @brief Copy data to the end of the stored array
+     * @brief Get a reference to the element in the stored object with the
+     * specified key. Convert to an object value if a value is not being
+     * stored. Insert the element if it does not exist and an object is being
+     * stored
+     *
+     * @param key The key of the element
+     * @return The reference to the element
+     *
+     * @throws std::bad_variant_access If the stored value is not an object
+     * @throws std::out_of_range If the object does not have an element with the
+     * specified key
+     */
+    [[nodiscard]] auto operator [](std::string_view key) -> json&;
+
+    /**
+     * @brief Copy data to the end of the stored array. Convert to an array
+     * value if the value being stored is not an array
      *
      * @param node The data to add
      */
     auto push_back(const json& node) -> void;
 
     /**
-     * @brief Move data to the end of the stored array
+     * @brief Move data to the end of the stored array. Convert to an array
+     * value if the value being stored is not an array
      *
      * @param node The data to add
      */
@@ -350,12 +354,19 @@ class json final {
     // ----------- print ------------
 
     /**
-     * @brief Encode the stored value into json
+     * @brief Encode the stored value to a stream
      *
      * @param indent The amount of spaces to use for indentation
      */
     [[nodiscard]] auto encode(std::size_t indent = 4) const -> std::string;
 
+    /**
+     * @brief Encode the stored value to a stream. The setw operator has been
+     * overridden to represent indentation
+     *
+     * @param os The output stream
+     * @param node The input node
+     */
     friend auto operator <<(std::ostream& os, const json& node)
         -> std::ostream&;
 
